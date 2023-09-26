29b88d4f2ea72d88d1db66ab4d02de790fdb0379
Anna Wójcik <aniawojcik894@gmail.com>
2023-09-26 20:54:23 +0200

    adding amount and choosing currency interactions

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,40 +1,70 @@
+import { useState } from "react";
+import { currencies } from "../currencies";
 import "./style.css";
 
-const Form = () => (
-    <form>
-        <fieldset className="form__fieldset">
-            <legend className="form__legend">Kalkulator walutowy</legend>
-            <p>
-                <label>
-                    <span className="form__labelText">
-                        Wpisz kwotę*:
-                    </span>
-                    <input className="form__field" type="number" name="amount" step="0.01"
-                        min="0" required />
-                </label>
-            </p>
-            <p>
-                <label>
-                    <span className="form__labelText">
-                        Waluta:
-                    </span>
-                    <select className="form__field" name="currency">
-                        <option value="EUR">EUR - Unia Europejska</option>
-                        <option value="USD">USD - USA</option>
-                        <option value="GBP">GBP - W. Brytania</option>
-                        <option value="CHF">CHF - Szwajcaria</option>
-                        <option value="AUD">AUD - Australia</option>
-                    </select>
-                </label>
-            </p>
-            <p>Aktualny kurs:<span className="form__paragraph--course"></span>
-            </p>
-            <button className="form__button">Przelicz</button>
-            <p className="form__paragraph--centered">Otrzymujesz:<span></span> PLN.
-            </p>
-            <p className="form__paragraph--fontSmall">Pola wymagane oznaczone są *</p>
-        </fieldset>
+const Form = () => {
+  const [currency, setCurrency] = useState(currencies[0].shortName);
+  const [amount, setAmount] = useState("");
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form onSubmit={onFormSubmit}>
+      <fieldset className="form__fieldset">
+        <legend className="form__legend">Kalkulator walutowy</legend>
+        <p>
+          <label>
+            <span className="form__labelText">
+              Wpisz kwotę w zł*:
+            </span>
+            <input
+              value={amount}
+              onChange={({ target }) => setAmount(target.value)}
+              className="form__field"
+              type="number"
+              name="amount"
+              step="0.01"
+              min="0"
+              autoFocus
+              required
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            <span className="form__labelText">
+              Waluta:
+            </span>
+            <select
+              value={currency}
+              onChange={({ target }) => setCurrency(target.value)}
+              className="form__field"
+              name="currency"
+            >
+              {currencies.map(currency => (
+                <option
+                  key={currency.shortName}
+                  value={currency.shortName}
+                >
+                  {currency.name}
+                </option>
+              ))}
+            </select>
+          </label>
+        </p>
+        <p>
+          Aktualny kurs:<span className="form__paragraph--course"></span>
+        </p>
+        <button className="form__button">Przelicz</button>
+        <p className="form__paragraph--centered">
+          Otrzymujesz:<span></span> PLN.
+        </p>
+        <p className="form__paragraph--fontSmall">Pola wymagane oznaczone są *</p>
+      </fieldset>
     </form>
-);
+  );
+};
 
 export default Form;
