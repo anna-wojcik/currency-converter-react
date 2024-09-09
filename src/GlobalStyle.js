254ebe2a55c842bce9bd977e55bd88715d29aee1
Anna Wójcik <aniawojcik894@gmail.com>
2024-09-09 11:58:27 +0200

    GlobalStyle.js file added

---
+++ src/GlobalStyle.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+import background from "./background-image.jpg";
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+        box-sizing: border-box;
+    }
+    
+    *, ::after, ::before {
+        box-sizing: inherit;
+    }
+
+    body {
+        font-family: "Lato", sans-serif;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        min-height: 100vh;
+        background-image: url("${background}");
+        background-size: cover;
+        background-position: center;
+    }   
+`;
