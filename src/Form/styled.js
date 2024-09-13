63f8bc8602284b1b0f8872014f849993ef9a1547
Anna Wójcik <aniawojcik894@gmail.com>
2024-09-13 20:36:43 +0200

    styles of Form component changed

--- src/Form/styled.js
+++ src/Form/styled.js
@@ -1,27 +1,7 @@
 import styled, { css } from "styled-components";
 
-export const StyledForm = styled.form`
-    width: 500px;
-
-    @media(max-width: ${({ theme }) => theme.breakpoint.mobileSpecial}px){
-        width: 90%;
-    }
-`;
-
 export const Fieldset = styled.fieldset`
-    border: 2px solid ${({ theme }) => theme.color.silver};
-    border-radius: 10px;
-    padding: 40px;
-    margin: 20px 0;
-    background-color: ${({ theme }) => theme.color.white};
-    box-shadow: 0 0 30px ${({ theme }) => theme.color.black};
-`;
-
-export const Legend = styled.legend`
-    background-color: ${({ theme }) => theme.color.cadetblue};
-    border-radius: 10px;
-    padding: 10px;
-    color: ${({ theme }) => theme.color.white};
+    border: none;
 `;
 
 export const Label = styled.label`
@@ -50,13 +30,22 @@ export const UniversalFormElement = styled.input`
 `;
 
 export const Paragraph = styled.p`
+    ${({ colorCadetBlue }) => colorCadetBlue && css`
+        color: ${({theme}) => theme.color.cadetblue}
+    `}
+
+    ${({ colorRed }) => colorRed && css`
+        color: ${({theme}) => theme.color.red}
+    `}
+
     ${({ centered }) => centered && css`
         padding: 10px;
         text-align: center;
     `}
 
     ${({ fontSmall }) => fontSmall && css`
-        font-size: 13px;
+        font-size: 14px;
+        color: ${({ theme }) => theme.color.scorpion};
     `}
 ;`
 
@@ -67,7 +56,7 @@ export const Button = styled.button`
     border-radius: 10px;
     padding: 10px 60px;
     display: block;
-    margin: 40px auto;
+    margin: 0px auto;
 
     &:hover {
         filter: brightness(110%);
